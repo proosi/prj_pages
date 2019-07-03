90d12d143203bdd6bc8be9c93bf63c95494ed320
Marek Osiński <osimara67@gmail.com>
2019-07-03 13:59:54 +0200

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPage(TemplateView):
+    template_name = 'pages/about.html'
